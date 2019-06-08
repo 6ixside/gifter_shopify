5570e62c7f40b91a396bf9d4645ec08eabdac6dd
hunterls97 <hunter.schofield@york.ca>
2019-06-08 13:15:04 -0400

    cleanup and reorganization

--- app.js
+++ app.js
@@ -6,102 +6,34 @@ var dontenv = require('dotenv').config()
 
 var app = express();
 
-var apiKey = process.env.API_KEY;
-var apiSecret = process.env.API_SECRET;
-var appUrl = "https://76bbfe42.ngrok.io";
+var tokens = {}
+const index = require('./server/routes/index')(tokens);
+const install = require('./server/routes/install')(tokens);
 
-let authentication = require('./server/middlewares/authentication.js')(apiKey, apiSecret);
 
-var scopes = 'write_script_tags'
-tokens = {};
-
-app.get('/', (req, res, next) =>{
-  res.send('test2');
-})
-
-app.get('/gifter/install', (req, res, next) => {
-	var shop = req.query.shop;
-	
-	if(shop){
-		var state = nonce()(); //idk why nonce now needs to be called like this??
-    var redir = appUrl + '/gifter/setup';
-
-    var installUrl = 'https://' + shop +
-      '/admin/oauth/authorize?client_id=' + apiKey +
-      '&scope=' + scopes +
-      '&state=' + state +
-      '&redirect_uri=' + redir;
-	
-  	res.cookie('state', state);
- 		res.redirect(installUrl);
-	}
-	else
-		res.status(400).send('missing shop parameter');
-});
-
-app.get('/gifter/setup', async (req, res, next) =>{
+/*app.get('/gifter/auth', async (req, res, next) =>{
   var shop = req.query.shop;
-  var scriptTagUrl = 'https://' + shop + '/admin/script_tags.json';
 
   if(tokens[shop] == undefined){
     await authentication.getAccessToken(req, res, next).then((token) =>{
       tokens[shop] = token;
+      console.log(tokens);
+
+      res.status(200).send('this is where the admin panel will go!');
     }, (err) =>{
       console.log(err);
     });
-
-    let headers = {
-      'X-Shopify-Access-Token': tokens[shop]
-    }
-
-    let body = {
-      "script_tag": {
-        "event": 'onload',
-        "src": appUrl + '/test'
-      }
-    }
-
-    request.post({
-      url: scriptTagUrl,
-      headers: headers,
-      body: body,
-      json: true
-    }).then((shopRes) => {
-      console.log(shopRes);
-
-      request.get({
-        url: 'https://' + shop + '/admin/script_tags.json',
-        headers: headers
-      }).then((data) => {console.log(data);})
-    }, (err) =>{
-      console.log('error: ' + err);
-    });
   }
+});*/
 
-  next();
-  //res.redirect(appUrl + '/gifter/auth');
-}, (req, res, next) =>{
-  var shop = req.query.shop;
+app.use('/', index);
+app.use('/install', install);
 
-  res.status(200).send('<p>Admin Panel</p>');
+//handle 404
+app.use((req, res, next) =>{
+  var err = new Error('Page not found :(');
+  err.status = 404;
+  next(err);
 });
-
-app.get('/gifter/auth', (req, res, next) =>{
-  var shop = req.query.shop;
-
-  authentication.getAccessToken(req, res, next).then((token) =>{
-    tokens[shop] = token;
-
-    console.log(tokens)
-
-    res.status(200).send('this is where the admin panel will go!');
-  }, (err) =>{
-    console.log(err);
-  });
-});
-
-app.get('/test', (req, res, next) =>{
-  res.sendfile(__dirname + '/public/gifter.js');
-})
 
 app.listen(4567, () =>{console.log('running gifter')});
---
+++ server/routes/install.js
@@ -0,0 +1,100 @@
+var express = require('express');
+var nonce = require('nonce');
+var request = require('request-promise');
+var router = express.Router();
+
+var appUrl = "https://c45948f6.ngrok.io";
+var scopes = 'write_script_tags'
+
+var apiKey = process.env.API_KEY;
+var apiSecret = process.env.API_SECRET;
+const authentication = require('../middlewares/authentication.js')(apiKey, apiSecret);
+
+module.exports = (tokens) =>{
+
+	//main install route
+	router.get('/', (req, res, next) =>{
+		var shop = req.query.shop;
+	  console.log('running install');
+
+		if(shop){
+			var state = nonce()(); //idk why nonce now needs to be called like this??
+	    var redir = appUrl + '/install/setup';
+
+	    var installUrl = 'https://' + shop +
+	      '/admin/oauth/authorize?client_id=' + apiKey +
+	      '&scope=' + scopes +
+	      '&state=' + state +
+	      '&redirect_uri=' + redir;
+		
+	  	res.cookie('state', state);
+	 		res.redirect(installUrl);
+		}
+		else
+			res.status(400).send('missing shop parameter');
+	});
+
+	//install route callback
+	router.get('/setup', async (req, res, next) =>{
+	  var shop = req.query.shop;
+	  var scriptTagUrl = 'https://' + shop + '/admin/script_tags.json';
+	  console.log('running setup');
+
+	  if(tokens[shop] == undefined){
+	    await authentication.getAccessToken(req, res, next).then((token) =>{
+	      tokens[shop] = token;
+	    }, (err) =>{
+	      console.log(err);
+	    });
+
+	    let headers = {
+	      'X-Shopify-Access-Token': tokens[shop]
+	    }
+
+	    let body = {
+	      "script_tag": {
+	        "event": 'onload',
+	        "src": appUrl + '/install/test'
+	      }
+	    }
+
+	    request.post({
+	      url: scriptTagUrl,
+	      headers: headers,
+	      body: body,
+	      json: true
+	    }).then((shopRes) => {
+	      request.get({
+	        url: 'https://' + shop + '/admin/script_tags.json',
+	        headers: headers
+	      }).then((data) => {/*console.log(data);*/})
+	    }, (err) =>{
+	      console.log('error: ' + err);
+	    });
+	  }
+
+	  next();
+	  //res.redirect(appUrl + '/gifter/auth');
+	}, (req, res, next) =>{
+	  //create new company in gifter
+
+
+	  next();
+	}, (req, res, next) =>{
+	  var shop = req.query.shop;
+
+	  authentication.authenticate(req, res, next).then((data) =>{
+	    res.status(200).send('<p>Install Complete!</p>');
+	    tokens[shop] = shop;
+	  }, (err) =>{
+	    console.log(err);
+	  });
+	  
+	});
+
+	router.get('/test', (req, res, next) =>{
+	  res.sendfile(__dirname + '../../public/gifter.js');
+	});
+
+	return router;
+}
